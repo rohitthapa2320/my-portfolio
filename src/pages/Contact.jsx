ad6ae5d0be306e4fd3c906d9638e19b8e1716948
rohitthapa2320 <rohitthapa2362000@gmail.com>
2024-05-10 15:33:04 +0530

    Added Contact Support

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,114 @@
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const formRef= useRef(null);
+  const [form ,setForm]= useState({name: '', email: '', message:''});
+  const [isLoading, setIsLoading] = useState(false);
+
+  
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  };
+
+  const handleFocus = () => {
+
+  };
+  const handleBlur = () => {
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Rohit",
+        from_email: form.email,
+        to_email: 'andy@quora@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      //Show Success Message
+      //Hide Alert
+      setForm({name: '', email: '', message:''});
+    }).catch((error) => {
+      console.log(error);
+    })
+
+
+  };
+
+
   return (
-    <h1>Contact Page</h1>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get In Touch</h1>
+
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john@doe.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              className="textarea"
+              placeholder="Let me know how can I help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            rows={4}
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur} 
+          >
+            {isLoading ? "Sending...": "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
   )
 };
 
